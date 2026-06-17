b8493d2566932a21c38239ec459f3c595f82c3f2
Edinah00 <edinah24.aphrodite@gmail.com>
2026-06-17 08:41:59 +0300

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+//$routes->get('/', 'Home::index');
+// Routes.php
+$routes->get('/produits', 'Produit::index');
+// Controller
